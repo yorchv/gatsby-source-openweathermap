d378caff75570836513e32398cef6277cdb513ff
ftavares <fabio.martins.tavares@gmail.com>
2018-12-16 22:00:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# gatsby-source-pixabay
+
+This source plugin for Gatsby will make weather infomation from [OpenWeatherMap](https://openweathermap.org) available in GraphQL queries.
+
+## Installation
+
+```sh
+# Install the plugin
+yarn add gatsby-source-openweatherapi
+```
+
+In `gatsby-config.js`:
+
+```js
+module.exports = {
+  plugins: [
+ {
+      resolve: `gatsby-source-openweatherapi`,
+      options: {
+        apikey: 'YOUR KEY',
+        location: 'London,york',
+        units: 'metric',
+        type: 'forecast'
+      },
+    },
+  ]
+};
+```
+
+**NOTE:** To get a Pixabay API key, [register for a Pixabay account](https://home.openweathermap.org/users/sign_up).
+
+See the [OpenWeatherMap API docs](https://openweathermap.org/api).
---
+++ src/gatsby-node.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+const crypto = require('crypto');
+
+exports.sourceNodes = async (
+  { actions: { createNode }, createNodeId },
+  { plugins, ...options }
+) => {
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${options.type}?q=${options.location}&units=${options.units}&apikey=${options.apikey}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+
+  createNode({
+    ...data,
+    id: createNodeId(`weather-${data.id}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: 'OpenWeather',
+      content: JSON.stringify(data),
+      contentDigest: crypto
+        .createHash('md5')
+        .update(JSON.stringify(data))
+        .digest('hex'),
+    },
+  });
+};
